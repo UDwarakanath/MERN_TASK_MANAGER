fac90f97efbf663d5aceb41afa96098c844c8d07
UDwarakanath <dwarakanathummadisetti@gmail.com>
2023-09-20 11:58:20 +0530

    connected mongodb to our server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,29 @@
-const express = require("express")
+const express = require("express");
+const dotenv = require("dotenv").config()
+const connectDB = require("./config/connectDB")
+const app = express();
 
-const app = express()
+app.get("/", (req, res) => {
+  res.send(`<html>
+    <body>
+        <h1>Hey There hope you are doing Great</h1>
+    </body>
+  </html>`);
+});
 
-const PORT = process.env.POR || 5000
+const startServer = async ()=>{
+    try {
+        await connectDB()
+        const PORT = process.env.POR || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`Server running on Port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running on Port ${PORT}`);
+});
+
+    } catch (error) {
+        console.log("error at connection",error)
+    }
+}
+
+startServer()
 
